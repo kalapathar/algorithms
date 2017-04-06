5ef7d5ffeb4144e7d07a6c0d3d14be91d7269833
Deepak Shah <shah2@stolaf.edu>
2017-04-06 11:23:53 -0500

    Updated Hw18

---
+++ hw18/check.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+#include <vector>
+using namespace std;
+
+int main(){
+	
+	//vector<map<int, int> > Graph;
+	
+	//Graph[0].insert[0];
+	//cout<<Graph[0][0];
+	int v=10;
+	vector<int> dist(v,0); 
+	cout<<dist.size()<<endl;
+
+
+}
---
+++ hw18/heap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+int main(){
+	vector<int> v1={20,30,40,25,15};
+	make_heap(v1.begin(),v1.end());
+	cout<<"The maximum element of heap is: ";
+	cout<<v1.front()<<endl;
+
+
+}
