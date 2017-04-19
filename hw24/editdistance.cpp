0135892a7b2227745c6f39d1bec36f6dc2c992ff
Deepak Shah <shah2@stolaf.edu>
2017-04-18 23:52:46 -0500

    Updated HW 24

--- hw24/editdistance.cpp
+++ hw24/editdistance.cpp
@@ -2,31 +2,35 @@
 #include <algorithm>
 using namespace std;
 
+	
+string m="anagram";
+string n="agnar";
+
+int equal(int i, int j){
+	if (m[j-1]==n[i-1]) return 0;
+	return 1;
+}
+
 
 int main(){
-	string m="anagram";
-	string n="agnar";
-	int dist  [n.size()+1] [m.size()+1];
-	for (int i=0;i<m.size();i++){
+
+	int length=m.size();
+	int breadth=n.size();
+	int dist [breadth+1] [length+1];
+	for (int i=0;i<=length;i++){
 		dist[0][i]=i;
 	}
-	for (int i=0;i<n.size();i++){
+
+	for (int i=0;i<=breadth;i++){
 		dist[i][0]=i;
 	}
 
-	for(int i=1;i<n.size();i++){
-		for( int j=1;j<m.size();j++){
-			dist[i][j]=min(min(dist[i-1][j]+1, dist[i][j-1]+1), dist[i-1][j-1]+abs(i-j));
+	for(int i=1;i<=breadth;i++){
+		for( int j=1;j<=length;j++){
+			dist[i][j]=min(min(dist[i-1][j]+1, dist[i][j-1]+1), dist[i-1][j-1]+equal(i,j));
 		}
+		
 	}
 
-	for(int i=0;i<n.size();i++){
-		cout<<"--------------------------------------------"<<endl;
-		for( int j=0;j<m.size();j++){
-			cout<<dist[i][j];
-		}
-		cout<<"----------------------------------------------"<<endl;
-	}
-
-
+	cout<<"The edit distance for "<<m<<" and "<<n<<" is "<<dist[breadth][length]<<endl;
 }
